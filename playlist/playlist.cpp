c79e7e29e224219629005b8239ad25e55c4813cf
Hyunjoon Sung <sunghj1118@gmail.com>
2022-12-09 14:10:04 +0900

    Added exception handling for case when trying to display the playlist when the playlist is empty.

--- playlist/playlist.cpp
+++ playlist/playlist.cpp
@@ -4,41 +4,84 @@
 
 using namespace std;
 
+string list;
+vector<string> songs;
+
+void displayMenu();
+void addSong();
+void deleteSong();
+void playSong();
+void displayPlaylist();
+
+
 int main()
 {
-    string list;
-    vector<string> songs;
-    cout << "Name songs: \n";
+    int choice;
+    char confirm;
+
+    do
+    {
+        displayMenu();
+        cout << "Enter choice: " << endl;
+        cin >> choice;
+        
+        switch (choice) {
+        case 1: addSong(); break;
+        case 2: deleteSong(); break;
+        case 3: playSong(); break;
+        case 4: displayPlaylist(); break;
+        default: cout << "invalid choice" << endl;
+        }
+
+        cout << "\nReturn to menu? (Y/N)" << endl;
+        cin >> confirm;
+    } while (confirm == 'Y' || confirm == 'y');
+
+    return 0;
+}
+
+
+
+// Displays the main menu.
+void displayMenu() {
+    cout << "MENU\n";
+    cout << "1. Add songs.\n";
+    cout << "2. Delete songs.\n";
+    cout << "3. Play songs.\n";
+    cout << "4. Display playlist.\n\n";
+}
+
+// Adds a song(s) to the playlist.
+void addSong() {
+    cout << "\nName songs: \n";
     cout << "When finished say 'done'\n";
 
     do {
         getline(cin, list);
         songs.push_back(list);
     } while (list != "done");
+}
 
-    songs.pop_back();
+// Deletes a song(s) from the playlist.
+void deleteSong() {
+    cout << "Name songs to delete: ";
+}
 
-    cout << "\nYou have " << songs.size() << " songs in your playlist\n";
-
-    cout << "This is your playlist: \n";
-    for (int i = 0; i < songs.size(); i++)
-    {
-        cout << i + 1 << ". " << songs[i] << endl;
-    }
-
+// Plays a song in the playlist when selected.
+void playSong() {
     cout << "\nPlay song?\n";
     char checkcontinue;
     int currentsong;
 
     while (true) {
         cout << "\nEnter number of song to be played: " << endl;
-        
+
         try {
             cin >> currentsong;
             string test = songs.at(currentsong - 1);
             cout << "\nCurrently playing: " << songs[currentsong - 1] << endl;
         }
-        catch (exception &e) {
+        catch (exception& e) {
             cout << "There is no song entry for the number: " << currentsong << endl;
         }
 
@@ -47,7 +90,23 @@ int main()
         if (checkcontinue == 'N' || checkcontinue == 'n') {
             break;
         }
-    } 
+    }
+}
 
-    return 0;
+// Displays all the songs in the playlist.
+void displayPlaylist() {
+    cout << "\nYou have " << songs.size() << " songs in your playlist\n";
+    try {
+        string test = songs.at(0);
+        songs.pop_back();
+        
+        cout << "This is your playlist: \n";
+        for (int i = 0; i < songs.size(); i++)
+        {
+            cout << i + 1 << ". " << songs[i] << endl;
+        }
+    }
+    catch (exception& e) {
+        cout << "Add more songs to display." << endl;
+    }
 }
